982c3ba03dbe510efa0eeea52320d2589879b373
Kyle Hobbs <kyle.hobbs1@gmail.com>
2022-09-22 18:42:10 -0500

    create TopNav componnent

--- components/cards.js
+++ components/cards.js
@@ -3,7 +3,7 @@ import BingoCard from './bingo-card/bingo-card';
 function Cards(props) {
 
     return (
-        <div style={{marginBottom: '2rem'}}>
+        <div style={{marginBottom: '2rem', marginTop: '6rem'}}>
             {props.names.map((name) => {
                 const cardBooks = props.books.filter((book) => {
                     return book.user === name;
