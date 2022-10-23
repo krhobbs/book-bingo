62f55bcdcaef228e88fbe44cc724412a50142ee8
Kyle Hobbs <kyle.hobbs1@gmail.com>
2022-10-22 20:34:52 -0500

    styling fixes after user authentication update

--- components/bingo-card/bingo-item.tsx
+++ components/bingo-card/bingo-item.tsx
@@ -1,36 +1,49 @@
-import { useState } from "react";
-import ItemFront from "./item-front";
-import ItemBack from "./item-back";
+import { useState } from 'react';
+import ItemFront from './item-front';
+import ItemBack from './item-back';
 import { Box } from 'theme-ui';
 
 interface BingoItemProps {
-    user: string,
-    square: string,
-    bookReq: string,
-    book: {
-        title: string,
-        author: string
-    }
+  user: string;
+  square: string;
+  bookReq: string;
+  book: {
+    title: string;
+    author: string;
+  };
 }
 
 function BingoItem({ user, square, bookReq, book }: BingoItemProps) {
-    const [cardFlipped, setCardFlipped] = useState(false);
+  const [cardFlipped, setCardFlipped] = useState(false);
 
-    const handleFlip = () => {
-        setCardFlipped(!cardFlipped);
-    }
+  const handleFlip = () => {
+    setCardFlipped(!cardFlipped);
+  };
 
-    return (
-        <Box sx={{backgroundColor: book ? 'complete' : 'incomplete', display: 'flex', alignItems: 'center', justifyContent: 'center', textAlign: 'center', position: 'relative', blockSize: '130px', inlineSize: '100px', borderRadius: '15px', boxShadow: '4px 4px 15px -10px #000000'}} onClick={handleFlip}>
-            {cardFlipped ?
-                <ItemBack user={user} square={square} book={book} />
-            :
-                <ItemFront bookReq={bookReq} />
-            }
-        </Box>
-    );
-
+  return (
+    <Box
+      sx={{
+        backgroundColor: book ? 'complete' : 'incomplete',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        position: 'relative',
+        blockSize: ['98px', '138px'],
+        inlineSize: ['auto', '112px'],
+        borderRadius: '15px',
+        boxShadow: '4px 4px 15px -10px #000000',
+        padding: ['0.05rem', '0.1rem'],
+      }}
+      onClick={handleFlip}
+    >
+      {cardFlipped ? (
+        <ItemBack user={user} square={square} book={book} />
+      ) : (
+        <ItemFront bookReq={bookReq} />
+      )}
+    </Box>
+  );
 }
-
 
 export default BingoItem;
