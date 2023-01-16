ddc30699617fd3249642406db4cd1a1c1681983e
Kyle Hobbs <kyle.hobbs1@gmail.com>
2023-01-15 22:37:03 -0600

    feat: add error popup component and display error on client when user types incorrect password

--- components/login/login-form.tsx
+++ components/login/login-form.tsx
@@ -1,12 +1,14 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Box, Label, Input, Button, Text } from 'theme-ui';
 import { signIn } from 'next-auth/react';
 import Spacer from '../ui/Spacer';
 import Link from 'next/link';
+import ErrorPopup from '../ui/ErrorPopup';
 
-function LoginForm(props) {
+function LoginForm() {
   const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState('');
   const usernameInputRef = useRef<HTMLInputElement>();
   const passwordInputRef = useRef<HTMLInputElement>();
 
@@ -23,34 +25,54 @@ function LoginForm(props) {
     });
 
     if (!result.error) {
+      setErrorMessage('');
       router.replace('/profile');
+    } else {
+      setErrorMessage(result.error);
     }
   }
 
+  function closeErrorPopup() : void {
+    setErrorMessage('');
+  }
+
   return (
-    <Box sx={{ marginBlockStart: '7rem', mx: 'auto', maxInlineSize: '512px' }}>
-      <Box as="form" onSubmit={submitHandler}>
-        <Box>
-          <Label htmlFor="username">Username</Label>
-          <Input type="text" required id="username" ref={usernameInputRef} />
-        </Box>
-        <Spacer size={['2rem']} />
-        <Box>
-          <Label htmlFor="password">Password</Label>
-          <Input type="password" required id="password" ref={passwordInputRef} />
+    <>
+      <Box
+        sx={{ marginBlockStart: '7rem', mx: 'auto', maxInlineSize: '512px' }}
+      >
+        <Box as="form" onSubmit={submitHandler}>
+          <Box>
+            <Label htmlFor="username">Username</Label>
+            <Input type="text" required id="username" ref={usernameInputRef} />
+          </Box>
+          <Spacer size={['2rem']} />
+          <Box>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              required
+              id="password"
+              ref={passwordInputRef}
+            />
+          </Box>
+          <Spacer size={['2.4rem']} />
+          <Box>
+            <Button>Login</Button>
+          </Box>
         </Box>
         <Spacer size={['2.4rem']} />
-        <Box>
-          <Button>Login</Button>
-        </Box>
+        <Link href="/register">
+          <a>
+            <Text variant="link">Don&apos;t have an account? Register</Text>
+          </a>
+        </Link>
       </Box>
-      <Spacer size={['2.4rem']} />
-      <Link href="/register">
-        <a>
-          <Text variant='link'>Don&apos;t have an account? Register</Text>
-        </a>
-      </Link>
-    </Box>
+      {errorMessage && <Box>
+        <Spacer size={['2.4rem']} />
+        <ErrorPopup message={errorMessage} close={closeErrorPopup} sx={{margin: 'auto'}}></ErrorPopup>
+      </Box>}
+    </>
   );
 }
 
