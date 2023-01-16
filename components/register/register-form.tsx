136ac4bffd603a810ed85039073582686b4cf7a7
Kyle Hobbs <kyle.hobbs1@gmail.com>
2023-01-15 23:19:00 -0600

    add client side error messaging ui to the register form

--- components/register/register-form.tsx
+++ components/register/register-form.tsx
@@ -1,22 +1,24 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Box, Label, Input, Button, Text } from 'theme-ui';
 import Spacer from '../ui/Spacer';
 import Link from 'next/link';
+import ErrorPopup from '../ui/ErrorPopup';
 
 async function registerUser(username: string, password: string) {
   const response = await fetch('/api/auth/register', {
     method: 'POST',
-    body: JSON.stringify({username, password}),
+    body: JSON.stringify({ username, password }),
     headers: {
-      'Content-Type': 'application/json'
-    }
-  })
+      'Content-Type': 'application/json',
+    },
+  });
 
-  const data = response.json();
+  const data = await response.json();
+
 
   if (!response.ok) {
-    throw new Error('Error.')
+    throw new Error(data.message);
   }
 
   return data;
@@ -24,6 +26,7 @@ async function registerUser(username: string, password: string) {
 
 function RegisterForm(props) {
   const router = useRouter();
+  const [errorMessage, setErrorMessage] = useState('');
   const usernameInputRef = useRef<HTMLInputElement>();
   const passwordInputRef = useRef<HTMLInputElement>();
 
@@ -35,39 +38,60 @@ function RegisterForm(props) {
 
     try {
       const result = await registerUser(enteredUsername, enteredPassword);
+      setErrorMessage('');
       router.push('/login');
     } catch (error) {
-      console.log(error);
+      setErrorMessage(error.message);
     }
   }
 
+  function closeErrorPopup(): void {
+    setErrorMessage('');
+  }
+
   return (
-    <Box sx={{ marginBlockStart: '7rem', mx: 'auto', maxInlineSize: '512px' }}>
+    <>
       <Box
-        as="form"
-        onSubmit={submitHandler}
+        sx={{ marginBlockStart: '7rem', mx: 'auto', maxInlineSize: '512px' }}
       >
-        <Box>
-          <Label htmlFor="username">Username</Label>
-          <Input type="text" required id="username" ref={usernameInputRef} />
-        </Box>
-        <Spacer size={['2rem']} />
-        <Box>
-          <Label htmlFor="password">Password</Label>
-          <Input type="password" required id="password" ref={passwordInputRef} />
+        <Box as="form" onSubmit={submitHandler}>
+          <Box>
+            <Label htmlFor="username">Username</Label>
+            <Input type="text" required id="username" ref={usernameInputRef} />
+          </Box>
+          <Spacer size={['2rem']} />
+          <Box>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              required
+              id="password"
+              ref={passwordInputRef}
+            />
+          </Box>
+          <Spacer size={['2.4rem']} />
+          <Box>
+            <Button>Register</Button>
+          </Box>
         </Box>
         <Spacer size={['2.4rem']} />
-        <Box>
-          <Button>Register</Button>
-        </Box>
+        <Link href="/login">
+          <a>
+            <Text variant="link">Already have an account? Login</Text>
+          </a>
+        </Link>
       </Box>
-      <Spacer size={['2.4rem']} />
-      <Link href="/login">
-        <a>
-          <Text variant='link'>Already have an account? Login</Text>
-        </a>
-      </Link>
-    </Box>
+      {errorMessage && (
+        <Box>
+          <Spacer size={['2.4rem']} />
+          <ErrorPopup
+            message={errorMessage}
+            close={closeErrorPopup}
+            sx={{ margin: 'auto' }}
+          ></ErrorPopup>
+        </Box>
+      )}
+    </>
   );
 }
 
