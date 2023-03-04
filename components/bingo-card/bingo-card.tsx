4612d221b88682b36b4806a42bfcdeeb7a8b3e4d
Kyle Hobbs <kyle.hobbs1@gmail.com>
2023-03-04 14:25:41 -0600

    add type to bingo card square

--- components/bingo-card/bingo-card.tsx
+++ components/bingo-card/bingo-card.tsx
@@ -2,6 +2,15 @@ import BingoItem from './bingo-item';
 import Spacer from '../ui/Spacer';
 import { Box, Text } from 'theme-ui';
 
+interface BingoSquareProps {
+    id: string;
+    req: string;
+    book?: {
+        title: string;
+        author: string;
+    }
+}
+
 function BingoCard(props) {
 
     return (
@@ -10,7 +19,7 @@ function BingoCard(props) {
             <Spacer size={['1.25rem', '1.5rem']} />
             <Box sx={{display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gridTemplateRows: 'repeat(5, auto)', gap: ['0.3rem', '0.5rem']}}>
                 {
-                    props.card.squares.map((square) => {
+                    props.card.squares.map((square: BingoSquareProps) => {
                         return (
                             <BingoItem key={square.id} user={props.card.user} square={square.id} bookReq={square.req} book={square.book}/>
                         )
