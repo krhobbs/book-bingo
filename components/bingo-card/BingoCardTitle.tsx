8bf13029790ce3d79ce6f77e90af7b09aeba62a0
Kyle Hobbs <kyle.hobbs1@gmail.com>
2023-04-23 20:40:01 -0500

    split out the bingo card title and bingo card components

---
+++ components/bingo-card/BingoCardTitle.tsx
@@ -0,0 +1,67 @@
+import Spacer from '../ui/Spacer';
+import { Box, Button, Text } from 'theme-ui';
+import {
+  ArchiveBoxIcon,
+  ArrowUturnLeftIcon,
+} from '@heroicons/react/24/outline';
+import { useRouter } from 'next/router';
+
+interface BingoCardTitleProps {
+  username: string;
+  usersCard: boolean;
+  archived: boolean;
+  onArchiveCard: Function;
+  onUnarchiveCard: Function;
+}
+
+function BingoCardTitle({
+  username,
+  usersCard,
+  archived,
+  onArchiveCard,
+  onUnarchiveCard,
+}: BingoCardTitleProps) {
+  const router = useRouter();
+
+  return (
+    <Box sx={{ alignItems: 'center', display: 'flex', gap: '1.5rem' }}>
+      <Text variant={'heading2'}>{username || 'No name'}</Text>
+      {usersCard && !archived && router.asPath !== '/' && (
+        <>
+          <Button
+            sx={{
+              backgroundColor: 'primary',
+              color: 'text',
+              cursor: 'pointer',
+              padding: '0px',
+              inlineSize: ['38px', '60px'],
+              blockSize: ['16px', '26px'],
+            }}
+            onClick={() => onArchiveCard}
+          >
+            <ArchiveBoxIcon style={{ blockSize: '98%' }} />
+          </Button>
+        </>
+      )}
+      {usersCard && archived && (
+        <>
+          <Button
+            sx={{
+              backgroundColor: 'primary',
+              color: 'text',
+              cursor: 'pointer',
+              padding: '0px',
+              inlineSize: ['38px', '60px'],
+              blockSize: ['16px', '26px'],
+            }}
+            onClick={() => onUnarchiveCard}
+          >
+            <ArrowUturnLeftIcon style={{ blockSize: '80%' }} />
+          </Button>
+        </>
+      )}
+    </Box>
+  );
+}
+
+export default BingoCardTitle;
