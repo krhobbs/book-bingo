0d2345a9eed8d8c8e8d5ba1eb5a60057de361dbe
Kyle Hobbs <kyle.hobbs1@gmail.com>
2023-04-28 20:10:52 -0500

    fix color input on safari browser

--- components/layout/pages/AddBookLayout.tsx
+++ components/layout/pages/AddBookLayout.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import AddBookForm from '../../add-book/add-book-form';
+import AddBookForm from '../../add-book/AddBookForm';
 
 interface AddBookLayoutProps {
   cardId: string;
