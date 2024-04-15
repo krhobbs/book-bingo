64ebc20f8117ede118efdfa659cfa6a9dccb3448
Kyle Hobbs <kyle.hobbs01@gmail.com>
2024-04-14 21:51:40 -0500

    remove static props from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,5 @@
 import HomeLayout from '../components/layout/pages/HomeLayout';
-import { getAllCards } from '../utils/db-utils';
 
-export default function Home({ cards, pageCount }: { cards: Card[], pageCount: number }) {
-  return <HomeLayout cards={cards} pageCount={pageCount} />;
-}
-
-export async function getStaticProps() {
-  try {
-    const [cards, pageCount] = await getAllCards();
-
-    return {
-      props: {
-        cards: cards,
-        pageCount: pageCount
-      },
-      revalidate: 800,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {
-        cards: [],
-      },
-      revalidate: 800
-    };
-  }
+export default function Home() {
+  return <HomeLayout />;
 }
